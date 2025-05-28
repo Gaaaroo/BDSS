e9540d959d7d991f0153634110f5940383478051
Duic <ducvvm4a@gmail.com>
2025-05-28 15:07:03 +0700

    fix register page

---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function RegisterForm() {
+      const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    username: "",
+    phone: "",
+    // passwordConfirmed: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+    setError("");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!form.email || !form.password) {
+      setError("Please enter both email and password.");
+      return;
+    }
+
+    // Authentication logic here
+    console.log(form);
+
+    try {
+      const response = await fetch("http://localhost:8080/bdss/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", //json format
+        },
+        body: JSON.stringify(form),
+      });
+      const data = await response.json();
+      console.log("Login successful:", data);
+    } catch (err) {
+      console.error("Error during login:", err);
+      setError("Login failed. Please try again.");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm"
+      >
+        <h2 className="text-2xl font-bold mb-6 text-center">Sign up</h2>
+        {error && (
+          <div className="mb-4 text-red-600 text-sm text-center">{error}</div>
+        )}
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-0.5">Username</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter your username"
+            value={form.username}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 mb-0.5">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={form.email}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 mb-0.5">Phone</label>
+          <input
+            type="phone"
+            name="phone"
+            placeholder="Enter your phone number"
+            value={form.phone}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 mb-0.5">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            value={form.password}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 mb-0.5">Confirmed Password</label>
+          <input
+            type="password"
+            name="passwordConfirmed"
+            placeholder="Confirm your password"
+            value={form.passwordConfirmed}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
+        >
+          Register
+        </button>
+      </form>
+    </div>
+
+  );
+}
