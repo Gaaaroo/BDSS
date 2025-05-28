22581422801a3a8fc3c47c017d1420706759d81b
Duic <ducvvm4a@gmail.com>
2025-05-28 14:51:58 +0700

    add register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Register = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({
     email: "",
@@ -27,7 +27,7 @@ const Login = () => {
     // Authentication logic here
     console.log(form);
 
-    try {
+    try{
       const response = await fetch("http://localhost:8080/bdss/users", {
         method: "POST",
         headers: {
@@ -37,22 +37,19 @@ const Login = () => {
       });
       const data = await response.json();
       console.log("Login successful:", data);
-    } catch (err) {
+    }catch (err) {
       console.error("Error during login:", err);
       setError("Login failed. Please try again.");
     }
   };
 
-  const handleRegisterClick = () => {
-    navigate("/register");
-  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <form
         onSubmit={handleSubmit}
         className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm"
       >
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center">Sign up</h2>
         {error && (
           <div className="mb-4 text-red-600 text-sm text-center">{error}</div>
         )}
@@ -80,19 +77,51 @@ const Login = () => {
             required
           />
         </div>
-        <div>
-          <button onClick={handleRegisterClick}>Don't have account?</button>
+        <div className="mb-6">
+          <label className="block text-gray-700 mb-2">Phone</label>
+          <input
+            type="phone"
+            name="phone"
+            placeholder="Enter your phone number"
+            value={form.phone}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
         </div>
-
+        <div className="mb-6">
+          <label className="block text-gray-700 mb-2">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            value={form.password}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        {/* <div className="mb-6">
+          <label className="block text-gray-700 mb-2">Confirmed Password</label>
+          <input
+            type="password"
+            name="passwordConfirmed"
+            placeholder="Confirm your password"
+            value={form.passwordConfirmed}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div> */}
         <button
           type="submit"
           className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
         >
-          Login
+          Register
         </button>
       </form>
     </div>
   );
 };
 
-export default Login;
+export default Register;
