b3478865879cf892d0ba3ad8afa7338cd9e52949
Duic <ducvvm4a@gmail.com>
2025-05-28 18:13:10 +0700

    introspect function

--- Backend/bdss/src/main/java/com/swp/bdss/controller/AuthenticationController.java
+++ Backend/bdss/src/main/java/com/swp/bdss/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.swp.bdss.controller;
 
+import com.nimbusds.jose.JOSEException;
 import com.swp.bdss.dto.request.AuthenticationRequest;
+import com.swp.bdss.dto.request.IntrospectRequest;
 import com.swp.bdss.dto.response.ApiResponse;
 import com.swp.bdss.dto.response.AuthenticationResponse;
+import com.swp.bdss.dto.response.IntrospectResponse;
 import com.swp.bdss.service.AuthenticationService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -27,6 +32,17 @@ public class AuthenticationController {
                 .code(6868)
                 .message(isAuthenticated.isAuthenticated() ? "Login successful" : "Login failed")
                 .data(isAuthenticated)
+                .build();
+
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var isValid = authenticationService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder()
+                .code(8686)
+                .message(isValid.isValid() ? "Token valid" : "Token invalid")
+                .data(isValid)
                 .build();
 
     }
---
+++ Backend/bdss/src/main/java/com/swp/bdss/entities/InvalidatedToken.java
@@ -0,0 +1,23 @@
+package com.swp.bdss.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class InvalidatedToken {
+    @Id
+    String token_id;
+    int user_id;
+    Date expiryTime;
+
+}
