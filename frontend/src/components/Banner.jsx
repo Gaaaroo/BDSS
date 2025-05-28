23a8a03b07bce16076b82786af854deb1024f42b
nmChaau <chaunmse180582@fpt.edu.vn>
2025-05-28 23:32:05 +0700

    update landingpage, create slider

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import img from "../assets/images/cover-photo.jpg";
 export default function Banner() {
   return (
-    <div className="relative w-full h-96 overflow-hidden">
+    <div className="relative w-full h-[450px] overflow-hidden">
       <div className="absolute inset-0 bg-white opacity-10"></div>
-      <img src={img} alt="banner" className="w-full h-96 object-cover " />
+      <img src={img} alt="banner" className="w-full h-[450px] object-cover " />
       <div className="absolute inset-0 flex flex-col justify-center items-center">
         <h1 className="text-white font-bold text-3xl md:text-5xl text-center mb-20">
           "The joy of saving lives is in your veins. Donate blood."
         </h1>
-        <div className="flex space-x-64 mb-4">
-          <button className="bg-gray-100 text-black text-[20px] px-4 py-2 rounded-3xl shadow font-bold transform transition-transform duration-200 hover:scale-110">
+        <div className="flex space-x-60 mb-8">
+          <button className="bg-gray-100 text-black text-[20px] px-4 py-2 rounded-3xl shadow font-bold w-52 transform transition-transform duration-200 hover:scale-110">
             Become a Donor
           </button>
-          <button className="bg-gray-100 text-black text-[20px] px-4 py-2 rounded-3xl shadow font-bold transform transition-transform duration-200 hover:scale-110">
+          <button className="bg-gray-100 text-black text-[20px] px-4 py-2 rounded-3xl shadow font-bold w-52 transform transition-transform duration-200 hover:scale-110">
             Become a Seeker
           </button>
         </div>
-        <button className="bg-gray-100 text-black text-[20px] px-4 py-2 rounded-3xl shadow font-bold transform transition-transform duration-200 hover:scale-110">
+        <button className="bg-gray-100 text-black text-[20px] px-4 py-2 rounded-3xl shadow font-bold w-52 transform transition-transform duration-200 hover:scale-110 mt-4">
           Blood type
         </button>
       </div>
--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,14 +1,18 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay, EffectFade } from "swiper/modules";
 
+import img1 from "../assets/images/slider-1.PNG";
+import img2 from "../assets/images/slider-2.PNG";
+import img3 from "../assets/images/slider-3.PNG";
+import img4 from "../assets/images/slider-4.PNG";
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/effect-fade";
 
-
 function ImageCarousel() {
   return (
-    <div className="relative group px-20 pb-20">
+    <>
       <Swiper
         modules={[Navigation, Autoplay, EffectFade]}
         spaceBetween={0}
@@ -18,19 +22,43 @@ function ImageCarousel() {
         autoplay={{ delay: 20000, disableOnInteraction: false, duration: 2000 }}
         effect="fade"
         fadeEffect={{ crossFade: true }}
-        className="w-full h-auto"
+        className="w-full h-[400px] group bg-white"
       >
-          <SwiperSlide >
-            
-          </SwiperSlide>
+        <SwiperSlide>
+          <h1 className="text-center text-7xl font-bold text-red-700 p-5">
+            A Snapshot of blood donation today
+          </h1>
+          <div className="flex flex-col md:flex-row items-center justify-center gap-80">
+            <img src={img4} alt="" className="w-38 h-38 object-contain" />
+            <img src={img3} alt="" className="w-38 h-38 object-contain" />
+            <img src={img2} alt="" className="w-38 h-38 object-contain" />
+            <img src={img1} alt="" className="w-38 h-38 object-contain" />
+          </div>
+          <div className="flex flex-col-4 items-center text-center gap-15 text-3xl text-gray-700 px-20 py-3">
+            <h3>
+              Every 2 seconds someone in America needs a blood transfusion
+            </h3>
+            <h3>The need is great extending beyond unexpected emergencies.</h3>
+            <h3>
+              3% of the population donates blood, but 100% of us will need it at
+              some point.
+            </h3>
+            <h3>Nearly 7 million people selflessly give blood every year.</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="text-4xl text-center justify-center">chưa xong</div>
+        </SwiperSlide>
       </Swiper>
       <style>
         {`
           .swiper-button-next,
           .swiper-button-prev {
-            opacity: 0;
-            transition: opacity 0.3s;
-            color: black;
+          margin: 0 30px;
+            
+            // opacity: 0;
+            // transition: opacity 0.3s;
+            color: gray;
           }
           .group:hover .swiper-button-next,
           .group:hover .swiper-button-prev {
@@ -38,7 +66,7 @@ function ImageCarousel() {
           }
         `}
       </style>
-    </div>
+    </>
   );
 }
 
