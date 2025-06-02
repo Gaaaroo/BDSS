5162a5c24f691370abe63228a7bb6b7c6565d7e7
Duic <ducvvm4a@gmail.com>
2025-06-02 20:23:09 +0700

    pùn ngủ quá nhm hình như setActiveUser dc ròi - 28

--- Backend/bdss/src/main/java/com/swp/bdss/controller/AuthenticationController.java
+++ Backend/bdss/src/main/java/com/swp/bdss/controller/AuthenticationController.java
@@ -1,10 +1,7 @@
 package com.swp.bdss.controller;
 
 import com.nimbusds.jose.JOSEException;
-import com.swp.bdss.dto.request.AuthenticationRequest;
-import com.swp.bdss.dto.request.IntrospectRequest;
-import com.swp.bdss.dto.request.LogoutRequest;
-import com.swp.bdss.dto.request.UserCreationRequest;
+import com.swp.bdss.dto.request.*;
 import com.swp.bdss.dto.response.ApiResponse;
 import com.swp.bdss.dto.response.AuthenticationResponse;
 import com.swp.bdss.dto.response.IntrospectResponse;
@@ -35,6 +32,16 @@ public class AuthenticationController {
                 .code(1111)
                 .message("OTP sent to your email")
                 .data(authenticationService.registerUserAndSendOtp(request))
+                .build();
+    }
+
+    @PostMapping("/verify")
+    ApiResponse<UserResponse> verifyOtp(@RequestBody VerifyOtpRequest request){
+        var user = authenticationService.verifyOtpAndActivateUser(request);
+        return ApiResponse.<UserResponse>builder()
+                .code(1000)
+                .message("User verified successfully")
+                .data(user)
                 .build();
     }
 
--- Backend/bdss/src/main/java/com/swp/bdss/repository/OtpCodeRepository.java
+++ Backend/bdss/src/main/java/com/swp/bdss/repository/OtpCodeRepository.java
@@ -1,6 +1,8 @@
 package com.swp.bdss.repository;
 
+import com.swp.bdss.entities.InvalidatedToken;
 import com.swp.bdss.entities.OtpCode;
+import com.swp.bdss.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import java.util.Optional;
 
     @Repository
     public interface OtpCodeRepository extends JpaRepository<OtpCode, Long> {
+        Optional<OtpCode> findByUserAndOtpCode(User user, String otpCode);
 
     }
