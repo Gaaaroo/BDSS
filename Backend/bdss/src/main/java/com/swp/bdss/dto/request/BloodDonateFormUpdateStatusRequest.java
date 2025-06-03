03413470e898a4f625f41209095dfe6219cfd2d7
Duic <ducvvm4a@gmail.com>
2025-06-03 14:16:43 +0700

    add updateDonateFormStatus function - 27

---
+++ Backend/bdss/src/main/java/com/swp/bdss/dto/request/BloodDonateFormUpdateStatusRequest.java
@@ -0,0 +1,17 @@
+package com.swp.bdss.dto.request;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BloodDonateFormCreationRequest {
+
+    String health_notes;
+}
