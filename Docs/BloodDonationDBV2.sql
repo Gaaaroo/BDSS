a2999dc5d4187cb6ae5844ee60bfa50f99cd4bae
HuynhNguyen2809 <kdshuynh48964@gmail.com>
2025-06-16 23:01:31 +0700

    !

--- Docs/BloodDonationDBV2.sql
+++ Docs/BloodDonationDBV2.sql
@@ -128,7 +128,7 @@ GO
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
     full_name VARCHAR(100),	 
     gender VARCHAR(10) CHECK (gender IN ('Male', 'Female')),
     dob DATE,
