724bad2fdc3e96f1bd32e88be8a2bdc1a75d43b1
HuynhNguyen2809 <kdshuynh48964@gmail.com>
2025-07-14 00:51:48 +0700

    đổi biến môi trường để deploy

--- Backend/bdss/src/main/java/com/swp/bdss/configuration/WebConfig.java
+++ Backend/bdss/src/main/java/com/swp/bdss/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // hoặc "/bdss/**"
-                .allowedOrigins("*")// frontend origin
+                .allowedOrigins("http://localhost:5173")// frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
